89a184ba0996f37f44876a5da138627b8d1955cf
LatentLeprechaun <latentleprechaun@gmail.com>
2014-03-13 14:32:15 -0700

    Tweaked CCLN until I figure something out

--- forge-1.6.4-9.11.1.953-src/mcp/src/minecraft/latentleprechaun/tutorial/CCLN.java
+++ forge-1.6.4-9.11.1.953-src/mcp/src/minecraft/latentleprechaun/tutorial/CCLN.java
@@ -5,14 +5,7 @@ import net.minecraft.entity.Entity;
 public class CCLN {
 	
 	public static void twizzle(Entity par1entity){
-		for(int n=0; n<3; n++){
-			par1entity.motionY = 0.2F + n;
-			par1entity.motionX = 0.2F + n;
-			par1entity.motionZ = 0.2F + n;
-			par1entity.motionX = -0.4F - n;
-			par1entity.motionZ = -0.4F - n;
-			par1entity.motionX = 0.4F + n;
-		}
+		par1entity.motionY = 0.8F;
 	}
 
 }
